57fcb764faebec1cb47726dbcfb7515f3c713a6c
= <=>
2021-02-08 15:45:24 -0500

    agrego electron-reload

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,7 +1,15 @@
 "use stric";
 const { app } = require("electron");
+const path = require("path");
 import { MainWindow } from "./MainWindow";
 import { MenuBar } from "./MenuBar";
+
+//habilita recarga activa de electron en entorno dev
+if (process.env.NODE_ENV !== "production") {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "../node_modules", "bin", "electron"),
+  });
+}
 
 function createWindow() {
   const window = new MainWindow();
